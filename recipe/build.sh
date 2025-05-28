16b5a0ac287cd52da2f66c3189f7e18209121952
Daniel Nachun <daniel.nachun@gmail.com>
2025-05-27 23:25:28 -0700

    fix CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
+export CFLAGS="${CFLAGS} -D_BSD_SOURCE"
 
 cargo-bundle-licenses \
     --format yaml \
